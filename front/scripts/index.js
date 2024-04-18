96cbb61fb1c8a1a9517f5b932d5bc86124a4f27a
Thomas HOLDERBAUM <thomas.holderbaum@regnology.net>
2024-04-18 14:50:57 +0200

    connect API and Hide kanap index

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,21 +1,27 @@
+//API Connect
+const response = await fetch("http://localhost:3000/api/products");
+const products = await response.json();
+
 const itemsContainer = document.getElementById("items");
 
-// Create DOM
-const link = document.createElement('a');
-const article = document.createElement('article');
-const articleImg = document.createElement('img');
-const articleTitle=document.createElement('h3');
-const articleText = document.createElement('p');
+for(let i = 0 ; i < products.length; i++){
+    const link = document.createElement('a');
+    const article = document.createElement('article');
+    const articleImg = document.createElement('img');
+    const articleTitle=document.createElement('h3');
+    const articleText = document.createElement('p');
 
-link.setAttribute("href", './production.html?id=42');
-articleImg.setAttribute("src", '../product01.jpg');
-article.setAttribute("alt", "Lorem ipsum dolor sit amet, Kanap name1");
-articleTitle.textContent = "Kanap name 1";
-articleText.classList.add("productDescription");
+    link.setAttribute("href", `./production.html?id=${products[i]._id}`);
+    articleImg.setAttribute("src", products[i].imageUrl);
+    article.setAttribute("alt", "Lorem ipsum dolor sit amet, Kanap name1");
+    articleTitle.textContent = products[i].name;
+    articleText.classList.add("productDescription");
 
-// add dom
-itemsContainer.appendChild(link);
-link.appendChild(article);
-article.appendChild(articleImg);
-article.appendChild(articleTitle);
-article.appendChild(articleText);
+    // add dom
+    itemsContainer.appendChild(link);
+    link.appendChild(article);
+
+    article.appendChild(articleImg);
+    article.appendChild(articleTitle);
+    article.appendChild(articleText);
+}
